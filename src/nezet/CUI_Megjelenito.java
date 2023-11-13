262c0447db18216c965d5180a52ad12dd692faa1
csolti.peter <csolti.peter@hp99win10.szaleziakk.local>
2023-11-13 09:18:40 +0100

    nezet: Megjelenito

--- src/nezet/CUI_Megjelenito.java
+++ src/nezet/CUI_Megjelenito.java
@@ -1,5 +1,36 @@
 package nezet;
 
+import java.util.Scanner;
+import modell.Teglalap;
+
 public class CUI_Megjelenito {
+    
+    private static Scanner sc = new Scanner(System.in);
+
+    private Teglalap tegla;
+    
+    public CUI_Megjelenito() {
+        inicializalas();
+        
+        feladat();
+    }
+
+    private void inicializalas() {
+        int a = bekerEgeszt("\'A\' oldal: ");
+        int b = bekerEgeszt("\'A\' oldal: ");
+        tegla = new Teglalap(a, b);
+    }
+
+    private int bekerEgeszt(String kerdes) {
+        System.out.print(kerdes);
+        return sc.nextInt();
+    }
+
+    private void feladat() {
+        System.out.println("A téglalap területe, kerülete: ");
+        System.out.printf("\tterület: %d\n", tegla.terulet());
+        System.out.printf("\tkerület: %d\n", tegla.kerulet());
+    }
+    
     
 }
