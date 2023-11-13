803f59381813ae1efffe10fc09db1779413e975d
csolti.peter <csolti.peter@hp99win10.szaleziakk.local>
2023-11-13 09:14:16 +0100

    modell: Teglalap

--- src/modell/Teglalap.java
+++ src/modell/Teglalap.java
@@ -1,5 +1,34 @@
 package modell;
 
 public class Teglalap {
+    private int a, b;
 
+    public Teglalap(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    public double terulet(){
+        return a*b;
+    }
+    
+    public double kerulet(){
+        return 2*(a+b);
+    }
 }
